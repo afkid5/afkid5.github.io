9371f664d7258be63971a40bf0367bfe1856a9aa
Colin <colin.boyer.kelly@gmail.com>
2018-12-04 22:24:19 -0500

    testing

---
+++ MapLayers.js
@@ -0,0 +1,58 @@
+map.on('load', function () {
+
+    map.addSource('Airports', {
+        type: 'geojson',
+        data: 'usa_airports.js'
+    });
+
+    map.addLayer({
+        "id": "United1",
+        "type": "circle",
+        "source": {
+            "type": "geojson",
+            "data": bikeThefts
+        },
+
+    });
+
+    map.setFilter('United1', ['==', 'UCR', '625'])
+
+
+    map.addLayer({
+        "id": "Southwest1",
+        "type": "circle",
+        "source": {
+            "type": "geojson",
+            "data": bikeThefts
+        },
+
+    });
+
+    map.setFilter('Southwest1', ['==', 'UCR', '615'])
+
+
+    map.addLayer({
+        "id": "Delta1",
+        "type": "circle",
+        "source": {
+            "type": "geojson",
+            "data": bikeThefts
+        },
+
+    });
+
+    map.setFilter('Delta1', ['==', 'UCR', '635'])
+
+
+    map.addLayer({
+        "id": "American1",
+        "type": "circle",
+        "source": {
+            "type": "geojson",
+            "data": bikeThefts
+        },
+
+    });
+
+    map.setFilter('American1', ['==', 'UCR', '645'])
+});
